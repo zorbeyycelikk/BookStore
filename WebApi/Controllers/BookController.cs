8710a918ebe880f930eed3f6f8c2105c2f4e6c35
[Zorbey] <[zorbeyycelikk@outlook.com]>
2023-09-27 12:52:03 +0300

    In memory database implemented 1

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -10,30 +10,7 @@ public class BookController : ControllerBase
     // Staticlerin  yasam döngüsü uygulama çalışınca başlar , bitince sonlanır.
     private static List<Book> BookList = new List<Book>()
     {
-        new Book()
-        {
-            Id = 1,
-            Title = "Lean Startup",
-            GenreId = 1, // Personal Growth
-            PageCount = 200,
-            PublishDate = new DateTime(2001, 06, 12)
-        },
-        new Book()
-        {
-            Id = 2,
-            Title = "Herland",
-            GenreId = 2, // Science Fiction
-            PageCount = 250,
-            PublishDate = new DateTime(2010, 05, 23)
-        },
-        new Book()
-        {
-            Id = 3,
-            Title = "Dune",
-            GenreId = 2, // Personal Growth
-            PageCount = 540,
-            PublishDate = new DateTime(2001, 12, 21)
-        }
+        
     }; // end- BookList funct
 
     [HttpGet]
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.DbOperations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<BookStoreDbContext>(options => options.UseInMemoryDatabase(databaseName: "BookStoreDb"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,6 +17,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    DataGenerator.Initialize(services);
 }
 
 app.UseHttpsRedirection();
